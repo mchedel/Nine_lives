b84529ab504f9f2abd0f9eaf41376e80da494e68
mchedel <mr.mchedel@yahoo.com>
2024-09-30 22:23:33 +0400

    Finalized

--- main.py
+++ main.py
@@ -6,11 +6,32 @@ clue = list("?????")
 heart_symbol = u'\u2764'
 guessed = False
 
-while lives > 9:
+rand_word = random.choice(words)
+print(rand_word)
+
+while lives > 0:
     print(clue)
     print("Lives left: ", heart_symbol * lives)
     guess = input("Guess letter or a whole word: ")
-    lives -= 1
+
+    if guess == rand_word:
+        guessed = True
+        clue = list(rand_word)
+        break
+    
+    elif guess in rand_word:
+        i = rand_word.index(guess)
+        clue[i] = guess
+            
+    else:
+        lives -= 1
+
+if guessed:
+    print(clue)
+else:
+    print("Game Over!")
+
+    
 
 
     
